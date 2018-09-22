05f05ca0bf7a1aa8c0bd755297d5eb6278902dde
razku <senkaotaku@gmail.com>
2018-09-22 19:09:57 +0200

    Inicio del proyecto
    Subido proyecto

---
+++ IDesignerCourse/Assets/Con Amor Para Sergio/Scripts/SceneChangerForIdiots.cs
@@ -0,0 +1,40 @@
+﻿/** 
+ *  @file    SceneChangerForIdiots.cs
+ *  @author  Diego Llorens (Gamedyx)
+ *  @date    07/10/2018  
+ *  @version 1.0 
+ *  
+ *  @brief This is an ultra basic scene changer, almost for idiots, you just 
+ *  need to link the ChangeScene function to a button event, with a parameter
+ *  and when you click that button on Play, well.. it will change the scene
+ *
+ *  
+ *  
+ */
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChangerForIdiots : MonoBehaviour {
+
+  #region Variables
+  #endregion
+  
+  #region SceneChangerForIdiotsMethods
+	public void ChangeScene(string scene)
+	{
+		SceneManager.LoadScene(scene);
+	}
+  #endregion
+  
+  #region UnityMethods
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+  
+  #endregion
+}
---
+++ IDesignerCourse/Assets/Scripts/Tent/TentDestroy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentDestroy : MonoBehaviour {
+    public Mesh NewMesh;
+
+    void OnTriggerEnter(Collider other)
+    {
+        GetComponent<MeshFilter>().mesh=NewMesh;
+    }
+}
